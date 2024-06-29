9d8a03e3534fe8a00e38c3cd1aa5500ce2f25760
Jemish gadhiya <jemishgadhiya97@gmail.com>
2024-06-30 02:14:56 +0530

    upload file changes

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ class App {
     //use router middleware
     this.app.use('/api', this.router);
 
-    const uploadsDirectory = path.join(__dirname, './uploads');
+    const uploadsDirectory = path.join(__dirname, './uploads/');
     console.log('Uploads Directory:', uploadsDirectory);
     this.app.use('/uploads', express.static(uploadsDirectory));
   }
